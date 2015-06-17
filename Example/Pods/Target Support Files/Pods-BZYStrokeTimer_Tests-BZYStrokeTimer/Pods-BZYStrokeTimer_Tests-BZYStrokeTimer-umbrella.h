595a6c937062f449fe358887bc2905411c13852d
startupthekid <conronb@gmail.com>
2015-06-17 19:41:16 -0400

    fixed file reference errors

--- Example/Pods/Target Support Files/Pods-BZYStrokeTimer_Tests-BZYStrokeTimer/Pods-BZYStrokeTimer_Tests-BZYStrokeTimer-umbrella.h
+++ Example/Pods/Target Support Files/Pods-BZYStrokeTimer_Tests-BZYStrokeTimer/Pods-BZYStrokeTimer_Tests-BZYStrokeTimer-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "BZYStrokeTimer.h"
+#import "BZYStrokeTimerDelegate.h"
 
 FOUNDATION_EXPORT double BZYStrokeTimerVersionNumber;
 FOUNDATION_EXPORT const unsigned char BZYStrokeTimerVersionString[];
